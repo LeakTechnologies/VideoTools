05e2eea541371ee598571b693b8ac305432aa5bc
Stu Leak <leaktechnologies@proton.me>
2026-02-12 15:37:05 -0500

    Fix switch parameter default values in build script
    - Remove invalid  defaults from switch parameters
    - PowerShell switches don't support default values like this

--- scripts/windows/build.ps1
+++ scripts/windows/build.ps1
@@ -6,8 +6,8 @@
 $OutputEncoding = [System.Text.Encoding]::UTF8
 
 param(
-    [switch]$Clean = $false,
-    [switch]$SkipTests = $false
+    [switch]$Clean,
+    [switch]$SkipTests
 )
 
 function Write-Header {
