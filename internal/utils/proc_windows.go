1ff07855f7de7dca90a428bfc5c2e5340e8c1f74
Stu Leak <leaktechnologies@proton.me>
2026-02-12 20:49:07 -0500

    Enhance ApplyNoWindow to fully suppress console windows
    - Add CreationFlags: 0x08000000 (CREATE_NO_WINDOW)
    - More effective at suppressing console popups on Windows
    - This function is used in 53+ places throughout the codebase

--- internal/utils/proc_windows.go
+++ internal/utils/proc_windows.go
@@ -12,5 +12,8 @@ func ApplyNoWindow(cmd *exec.Cmd) {
 	if cmd == nil {
 		return
 	}
-	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		HideWindow:    true,
+		CreationFlags: 0x08000000, // CREATE_NO_WINDOW - prevents console window
+	}
 }
