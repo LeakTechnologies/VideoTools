565fcb124795539c2b87ec126844d3dc578fb1dc
Stu Leak <leaktechnologies@proton.me>
2026-04-17 18:22:44 -0400

    feat(player): make volume slider vertical
    - Set volumeSlider.Orientation = widget.Vertical
    - Set size to 24x100 for a vertical bar
    - Fills from bottom up, easier to adjust than horizontal

--- internal/media/view.go
+++ internal/media/view.go
@@ -359,7 +359,9 @@ func (v *VideoPlayer) buildControls() {
 	v.volumeBtn.Resize(fyne.NewSize(36, 36))
 
 	v.volumeSlider = widget.NewSlider(0, 100)
+	v.volumeSlider.Orientation = widget.Vertical
 	v.volumeSlider.Value = v.volume * 100
+	v.volumeSlider.Resize(fyne.NewSize(24, 100))
 	v.volumeSlider.OnChanged = func(pos float64) {
 		v.volume = pos / 100.0
 		if v.onVolumeChange != nil {
